4ca7a288073835bd45b57308f0bd86b4810bd250
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-07-30 16:09:08 +0800

    Modify Package Name

---
+++ xrpc-api/src/main/java/com/smallc/xrpc/api/hello/HelloService.java
@@ -0,0 +1,12 @@
+package com.smallc.xrpc.api.hello;
+
+/**
+ * @author yj8023xx
+ * @version 1.0
+ * @since com.smallc.xrpc.api.hello
+ */
+public interface HelloService {
+
+    String hello(String name);
+
+}
