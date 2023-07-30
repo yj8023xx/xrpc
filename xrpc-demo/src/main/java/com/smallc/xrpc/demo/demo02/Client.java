7df9194b0bda24ffeec06fe5d83742780c5afc64
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-07-30 16:10:59 +0800

    Modify Package Name

---
+++ xrpc-demo/src/main/java/com/smallc/xrpc/demo/demo02/Client.java
@@ -0,0 +1,19 @@
+package com.smallc.xrpc.demo.demo02;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author zhang
+ * @version 1.0
+ * @since com.smallc.xrpc.demo.demo02
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring-client.xml");
+        HelloController controller = applicationContext.getBean(HelloController.class);
+        controller.say();
+    }
+
+}
