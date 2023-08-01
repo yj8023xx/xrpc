951009cc34c0565f5befbf44e5eb6544c9c19732
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-08-01 19:11:28 +0800

    Add support for cross language

--- xrpc-client/src/main/java/com/smallc/xrpc/client/stub/impl/ProxyStubFactory.java
+++ xrpc-client/src/main/java/com/smallc/xrpc/client/stub/impl/ProxyStubFactory.java
@@ -5,12 +5,14 @@ import com.smallc.xrpc.client.stub.ServiceStub;
 import com.smallc.xrpc.client.stub.StubFactory;
 import com.smallc.xrpc.common.loadbalancer.LoadBalancer;
 import com.smallc.xrpc.common.serializer.Serializer;
-import com.smallc.xrpc.network.request.RpcRequest;
+import com.smallc.xrpc.network.protocol.XRpcRequest;
 import com.smallc.xrpc.network.transport.Transport;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
 import java.lang.reflect.Proxy;
+import java.util.HashMap;
 
 /**
  * @author yj8023xx
@@ -32,11 +34,15 @@ public class ProxyStubFactory implements StubFactory {
         }
 
         @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            String interfaceName = method.getDeclaringClass().getCanonicalName();
+        public Object invoke(Object proxy, Method method, Object[] args) {
+            String serviceName = method.getDeclaringClass().getCanonicalName();
             String methodName = method.getName();
-            Class<?>[] parameterTypes = method.getParameterTypes();
-            RpcRequest request = new RpcRequest(interfaceName, methodName, parameterTypes, args);
+            HashMap<String, Object> argMap = new HashMap<>();
+            Parameter[] params = method.getParameters();
+            for (int i = 0; i < args.length; i++) {
+                argMap.put(params[i].getName(), args[i]);
+            }
+            XRpcRequest request = new XRpcRequest(serviceName, methodName, argMap);
             byte[] response = invokeRemote(loadBalancer.select(), serializer, request);
             return serializer.deserialize(response, method.getReturnType());
         }
