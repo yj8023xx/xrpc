b6a99796d89ac52b5f4232dc86903c3a25123860
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-08-01 19:13:04 +0800

    Add support for cross language

--- xrpc-client/src/main/java/com/smallc/xrpc/client/stub/ServiceStub.java
+++ xrpc-client/src/main/java/com/smallc/xrpc/client/stub/ServiceStub.java
@@ -3,7 +3,7 @@ package com.smallc.xrpc.client.stub;
 import com.smallc.xrpc.common.serializer.Serializer;
 import com.smallc.xrpc.common.utils.RpcRequestId;
 import com.smallc.xrpc.network.protocol.*;
-import com.smallc.xrpc.network.request.RpcRequest;
+import com.smallc.xrpc.network.protocol.XRpcRequest;
 import com.smallc.xrpc.network.transport.Transport;
 
 import java.util.concurrent.ExecutionException;
@@ -15,17 +15,17 @@ import java.util.concurrent.ExecutionException;
  */
 public interface ServiceStub {
 
-    default byte[] invokeRemote(Transport<XRpcMessage> transport, Serializer serializer, RpcRequest request) {
-        XRpcHeader header = new XRpcHeader(XRpcMessageType.RPC_REQUEST.getValue(), serializer.type().getValue(), RpcRequestId.next());
+    default byte[] invokeRemote(Transport<XRpcMessage> transport, Serializer serializer, XRpcRequest request) {
+        XRpcHeader header = new XRpcHeader((byte) XRpcMessageType.RPC_REQUEST.getValue(), (byte) serializer.type().getValue(), RpcRequestId.next(), (byte) XRpcStatus.SUCCESS.getValue());
         byte[] payload = serializer.serialize(request);
         XRpcMessage message = new XRpcMessage(header, payload);
         try {
             XRpcMessage response = transport.send(message).get();
-            XRpcResponseHeader responseHeader = (XRpcResponseHeader) response.getHeader();
-            if (responseHeader.getCode() == XRpcCode.SUCCESS.getValue()) {
+            XRpcHeader responseHeader = response.getHeader();
+            if (responseHeader.getStatus() == XRpcStatus.SUCCESS.getValue()) {
                 return response.getPayload();
             } else {
-                throw new Exception(responseHeader.getError());
+                throw new Exception(XRpcStatus.getContent(responseHeader.getStatus()));
             }
         } catch (ExecutionException e) {
             throw new RuntimeException(e.getCause());
