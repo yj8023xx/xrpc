876edb6c51aeac3aea87b68dffbe3026cd7b70af
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2023-08-01 19:08:11 +0800

    Add support for cross language

---
+++ xrpc-network/src/main/java/com/smallc/xrpc/network/protocol/XRpcConstant.java
@@ -0,0 +1,14 @@
+package com.smallc.xrpc.network.protocol;
+
+/**
+ * @author yj8023xx
+ * @version 1.0
+ * @since com.smallc.xrpc.network.protocol
+ */
+public class XRpcConstant {
+
+    public static final short MAGIC_NUMBER = (short) 0x8023;
+    public static final int VERSION = 1;
+    public static final int FIXED_HEADER_LENGTH = 18;
+
+}
---
+++ xrpc-network/src/main/java/com/smallc/xrpc/network/protocol/XRpcRequest.java
@@ -0,0 +1,48 @@
+package com.smallc.xrpc.network.protocol;
+
+import java.io.Serializable;
+import java.util.Map;
+
+/**
+ * RPC请求
+ *
+ * @author yj8023xx
+ * @version 1.0
+ * @since com.smallc.xrpc.network.request
+ */
+public class XRpcRequest implements Serializable {
+
+    private String serviceName;
+    private String methodName;
+    private Map<String, Object> argMap;
+
+    public XRpcRequest(String serviceName, String methodName, Map<String, Object> argMap) {
+        this.serviceName = serviceName;
+        this.methodName = methodName;
+        this.argMap = argMap;
+    }
+
+    public String getServiceName() {
+        return serviceName;
+    }
+
+    public void setServiceName(String serviceName) {
+        this.serviceName = serviceName;
+    }
+
+    public String getMethodName() {
+        return methodName;
+    }
+
+    public void setMethodName(String methodName) {
+        this.methodName = methodName;
+    }
+
+    public Map<String, Object> getArgMap() {
+        return argMap;
+    }
+
+    public void setArgMap(Map<String, Object> argMap) {
+        this.argMap = argMap;
+    }
+}
