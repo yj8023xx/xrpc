fc8d6e3d939f02f55955a8a2964d7a78f1381a0d
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2024-01-19 21:16:36 +0800

    Update XRpcServerGroup.java

--- xrpc-network/src/main/java/com/smallc/xrpc/network/transport/rdma/XRpcServerGroup.java
+++ xrpc-network/src/main/java/com/smallc/xrpc/network/transport/rdma/XRpcServerGroup.java
@@ -1,17 +1,10 @@
 package com.smallc.xrpc.network.transport.rdma;
 
-import com.ibm.disni.RdmaActiveCqProcessor;
-import com.ibm.disni.RdmaCqProcessor;
-import com.ibm.disni.RdmaCqProvider;
 import com.ibm.disni.RdmaEndpointFactory;
-import com.ibm.disni.verbs.IbvCQ;
-import com.ibm.disni.verbs.IbvContext;
-import com.ibm.disni.verbs.IbvQP;
 import com.ibm.disni.verbs.RdmaCmId;
 import com.smallc.xrpc.network.protocol.XRpcMessage;
 import com.smallc.xrpc.network.transport.RequestHandler;
 import com.smallc.xrpc.network.transport.RequestHandlerRegistry;
-import com.smallc.xrpc.network.transport.netty.NettyRequestInvocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -25,27 +18,26 @@ import java.io.IOException;
  */
 public class XRpcServerGroup extends XRpcEndpointGroup<XRpcServerEndpoint> {
 
-    private static final Logger logger = LoggerFactory.getLogger(NettyRequestInvocation.class);
+    private static final Logger logger = LoggerFactory.getLogger(XRpcServerGroup.class);
 
-    private int clusterCount;
-    private int curCluster;
-    private volatile RdmaCqProcessor[] cqProcessors;
-    private long[] affinity;
+    private XRpcResourceManager resourceManager;
     private RequestHandlerRegistry requestHandlerRegistry;
 
     private XRpcServerGroup(int timeout, RequestHandlerRegistry requestHandlerRegistry) throws IOException {
         super(timeout);
-        this.clusterCount = 1;
-        this.curCluster = 0;
-        this.affinity = new long[]{0};
+        this.resourceManager = new XRpcResourceManager(timeout);
         this.requestHandlerRegistry = requestHandlerRegistry;
     }
 
-    private XRpcServerGroup(int timeout, long[] affinity, RequestHandlerRegistry requestHandlerRegistry) throws IOException {
-        super(timeout);
-        this.clusterCount = affinity.length;
-        this.curCluster = 0;
-        this.affinity = affinity;
+    private XRpcServerGroup(int timeout, int threadCount, RequestHandlerRegistry requestHandlerRegistry) throws IOException {
+        super(timeout, threadCount);
+        this.resourceManager = new XRpcResourceManager(threadCount, timeout);
+        this.requestHandlerRegistry = requestHandlerRegistry;
+    }
+
+    private XRpcServerGroup(int timeout, long[] affinities, RequestHandlerRegistry requestHandlerRegistry) throws IOException {
+        super(timeout, affinities);
+        this.resourceManager = new XRpcResourceManager(timeout, affinities);
         this.requestHandlerRegistry = requestHandlerRegistry;
     }
 
@@ -55,53 +47,22 @@ public class XRpcServerGroup extends XRpcEndpointGroup<XRpcServerEndpoint> {
         return group;
     }
 
-    public static XRpcServerGroup createServerGroup(int timeout, long[] affinity, RequestHandlerRegistry requestHandlerRegistry) throws IOException {
-        XRpcServerGroup group = new XRpcServerGroup(timeout, affinity, requestHandlerRegistry);
+    public static XRpcServerGroup createServerGroup(int timeout, int threadCount, RequestHandlerRegistry requestHandlerRegistry) throws IOException {
+        XRpcServerGroup group = new XRpcServerGroup(timeout, threadCount, requestHandlerRegistry);
+        group.init(new XRpcServerFactory(group));
+        return group;
+    }
+
+    public static XRpcServerGroup createServerGroup(int timeout, long[] affinities, RequestHandlerRegistry requestHandlerRegistry) throws IOException {
+        XRpcServerGroup group = new XRpcServerGroup(timeout, affinities, requestHandlerRegistry);
         group.init(new XRpcServerFactory(group));
         return group;
     }
 
     @Override
-    public RdmaCqProvider createCqProvider(XRpcServerEndpoint endPoint) throws IOException {
-        IbvContext context = endPoint.getIdPriv().getVerbs();
-        if (null == context) {
-            throw new IOException("No context found!");
-        }
-        if (null == cqProcessors) {
-            createCqProcessors(context);
-        }
-        return cqProcessors[endPoint.getClusterId()];
-    }
-
-    private synchronized void createCqProcessors(IbvContext context) throws IOException {
-        if (null == cqProcessors) {
-            cqProcessors = new RdmaCqProcessor[clusterCount];
-            int cqSize = getMaxSendWr() + getMaxRecvWr();
-            int wrSize = getMaxSendWr() + getMaxRecvWr();
-            for (int i = 0; i < clusterCount; i++) {
-                cqProcessors[i] = new RdmaActiveCqProcessor(context, cqSize, wrSize, affinity[i], i, 1000, true);
-                cqProcessors[i].start();
-            }
-        }
-    }
-
-    @Override
-    public IbvQP createQpProvider(XRpcServerEndpoint endPoint) throws IOException {
-        IbvCQ cq = endPoint.getCqProvider().getCQ();
-        IbvQP qp = this.createQp(endPoint.getIdPriv(), endPoint.getPd(), cq);
-        cqProcessors[endPoint.getClusterId()].registerQP(qp.getQp_num(), endPoint);
-        return qp;
-    }
-
-    @Override
     public void allocateResources(XRpcServerEndpoint endPoint) throws Exception {
-        endPoint.allocateResources();
-    }
-
-    synchronized int newClusterId() {
-        int newClusterId = curCluster;
-        curCluster = (curCluster + 1) % clusterCount;
-        return newClusterId;
+        // Avoid blocking the main thread
+        resourceManager.allocateResources(endPoint);
     }
 
     @Override
@@ -125,6 +86,12 @@ public class XRpcServerGroup extends XRpcEndpointGroup<XRpcServerEndpoint> {
         } else {
             throw new Exception(String.format("No handler for request with type: %d!", request.getHeader().getMessageTypeId()));
         }
+    }
+
+    @Override
+    public synchronized void close() throws IOException, InterruptedException {
+        super.close();
+        this.resourceManager.close();
     }
 
     public static class XRpcServerFactory implements RdmaEndpointFactory<XRpcServerEndpoint> {
