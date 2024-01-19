a9ce99c629cc0c8a5d747a01292d35d4a35122e2
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2024-01-19 21:15:12 +0800

    Update XRpcClientEndpoint.java

--- xrpc-network/src/main/java/com/smallc/xrpc/network/transport/rdma/XRpcClientEndpoint.java
+++ xrpc-network/src/main/java/com/smallc/xrpc/network/transport/rdma/XRpcClientEndpoint.java
@@ -1,9 +1,6 @@
 package com.smallc.xrpc.network.transport.rdma;
 
-import com.ibm.disni.verbs.IbvCQ;
-import com.ibm.disni.verbs.IbvWC;
 import com.ibm.disni.verbs.RdmaCmId;
-import com.ibm.disni.verbs.SVCPollCq;
 import com.smallc.xrpc.network.protocol.XRpcMessage;
 import com.smallc.xrpc.network.transport.PendingRequests;
 import com.smallc.xrpc.network.transport.ResponseFuture;
@@ -22,40 +19,14 @@ public class XRpcClientEndpoint extends XRpcEndpoint {
 
     private static final Logger logger = LoggerFactory.getLogger(XRpcClientEndpoint.class);
 
-    private SVCPollCq poll;
-    private IbvWC[] wcList;
     private PendingRequests pendingRequests;
-    private CqProcessor cqProcessor;
 
     protected XRpcClientEndpoint(XRpcEndpointGroup group, RdmaCmId idPriv, boolean serverSide) throws IOException {
         super(group, idPriv, serverSide);
-        this.cqProcessor = new CqProcessor();
-    }
-
-    public void setPendingRequests(PendingRequests pendingRequests) {
-        this.pendingRequests = pendingRequests;
     }
 
     @Override
-    protected synchronized void init() throws IOException {
-        super.init();
-        IbvCQ cq = getCqProvider().getCQ();
-        this.wcList = new IbvWC[getCqProvider().getCqSize()];
-        for (int i = 0; i < wcList.length; i++) {
-            wcList[i] = new IbvWC();
-        }
-        this.poll = cq.poll(wcList, wcList.length);
-        this.cqProcessor.start();
-    }
-
-    @Override
-    public synchronized void close() throws IOException, InterruptedException {
-        super.close();
-        cqProcessor.close();
-    }
-
-    @Override
-    public void handleRecvEvent(XRpcMessage response) {
+    public void onMessageComplete(XRpcMessage response) {
         ResponseFuture<XRpcMessage> future = pendingRequests.remove(response.getHeader().getRequestId());
         if (null != future) {
             future.getFuture().complete(response);
@@ -64,43 +35,8 @@ public class XRpcClientEndpoint extends XRpcEndpoint {
         }
     }
 
-    private void pollOnce() throws IOException {
-        int ret = poll.execute().getPolls();
-        for (int i = 0; i < ret; i++) {
-            IbvWC wc = wcList[i];
-            dispatchCqEvent(wc);
-        }
-    }
-
-    private class CqProcessor implements Runnable {
-        private boolean running;
-        private Thread thread;
-
-        public CqProcessor() {
-            this.running = false;
-            this.thread = new Thread();
-        }
-
-        @Override
-        public void run() {
-            while (running) {
-                try {
-                    pollOnce();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        }
-
-        public void start() {
-            this.running = true;
-            thread.start();
-        }
-
-        public void close() throws InterruptedException {
-            this.running = false;
-            thread.join();
-        }
+    public void setPendingRequests(PendingRequests pendingRequests) {
+        this.pendingRequests = pendingRequests;
     }
 
 }
