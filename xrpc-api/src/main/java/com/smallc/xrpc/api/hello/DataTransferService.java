b5ac985149b6f0b3fce9743ab33ce3291b829486
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2024-01-23 11:39:52 +0800

    Upload xrpc-api

---
+++ xrpc-api/src/main/java/com/smallc/xrpc/api/hello/DataTransferService.java
@@ -0,0 +1,13 @@
+package com.smallc.xrpc.api.hello;
+
+/**
+ * @author yj8023xx
+ * @version 1.0
+ * @date 2024/1/16
+ * @since com.smallc.xrpc.api.hello
+ */
+public interface DataTransferService {
+
+    String send(String data);
+
+}
