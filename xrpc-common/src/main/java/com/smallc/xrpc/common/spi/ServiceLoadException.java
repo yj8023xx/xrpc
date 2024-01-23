42f033f22a7b78f3875777c2f19d36bec0756381
yj8023xx <46322393+yj8023xx@users.noreply.github.com>
2024-01-23 11:36:30 +0800

    Delete xrpc-common directory

--- xrpc-common/src/main/java/com/smallc/xrpc/common/spi/ServiceLoadException.java
+++
@@ -1,9 +0,0 @@
-package com.smallc.xrpc.common.spi;
-
-/**
- * @author yj8023xx
- * @version 1.0
- * @since com.smallc.xrpc.common.spi
- */
-public class ServiceLoadException extends RuntimeException {
-}
